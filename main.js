03cc60928d545247644630d2f2758f8d5d34793f
cstewart91 <cstewa91@gmail.com>
2022-03-22 21:42:27 -0700

    Added hit detection

--- main.js
+++ main.js
@@ -2,12 +2,10 @@ $(document).ready(initApp);
 
 function initApp() {
     $(document).on("keydown", movePlayer);
+    createPlayer()
     // setInterval(function () {
-        spawnBalls()
-    // }, 50000)
-    moveBall()
-    // checkIfHit()
-    checkPositions()
+    spawnBalls()
+    // }, 1000)
 }
 
 let level = 1;
@@ -17,9 +15,17 @@ let moveRightKeys = { key: 68, arrow: 39 };
 let moveDownKeys = { key: 83, arrow: 40 };
 let moveLeftKeys = { key: 65, arrow: 37 };
 
+function createPlayer() {
+    var player = $("#player");
+    var ctx = player[0].getContext('2d');
+    ctx.fillStyle = "blue";
+    ctx.fillRect(0, 0, 30, 30);
+    console.log(ctx)
+}
+
 function movePlayer(event) {
     let currentKeyPressed = event.which
-    console.log(currentKeyPressed)
+
     if (currentKeyPressed == moveUpKeys.key || currentKeyPressed == moveUpKeys.arrow) {
         moveUp();
     }
@@ -71,31 +77,47 @@ function spawnBalls() {
         'top': `${randomVerticalNumber}%`
     })
 
-    const interval = setInterval(function() {
-        console.log($(newBall).find())
-        // console.log($(newBall).position())
-        clearInterval(interval)
-    })
-
-    setTimeout(function () {
-        moveBall(newBall)
-    }, 1000)
+    moveBall(newBall)
+    checkPositions(newBall)
 
 }
 
 function moveBall(newBall) {
-    $(newBall).animate({left: '100%'}, 10000, "linear")
-    $(newBall).queue(function() {
+    $(newBall).animate({ left: '100%' }, 1000, "linear")
+    $(newBall).queue(function () {
         $(this).remove()
     })
 }
 
-function checkPositions() {
-
+function checkPositions(ball) {
+    setTimeout(function() {
+        const interval = setInterval(function () {
+            let ballPosition = $(ball).position()
+            checkIfHit(ballPosition)
+            if (ballPosition.left == 0) {
+                clearInterval(interval)
+            }
+        })
+    }, 100)
 }
 
-function checkIfHit() {
-    console.log($('.player').position())
-    console.log($('.ball').position())
-    if ($('.player').position()) { }
+function checkIfHit(ballPosition) {
+    let playerPosition = $('.player').position()
+    let playerHeight = $('.player').height()
+    let playerWidth = $('.player').width()
+    let playerTop = playerPosition.top
+    let playerLeft = playerPosition.left
+    let playerBottom = playerTop + playerHeight
+    let playerRight = playerLeft + playerWidth
+
+    let ballHeight = $('.ball').height()
+    let ballWidth = $('.ball').width()
+    let ballTop = ballPosition.top
+    let ballLeft = ballPosition.left
+    let ballBottom = ballTop + ballHeight
+    let ballRight = ballLeft + ballWidth
+
+    if(playerRight >= ballLeft && playerLeft <= ballRight && playerBottom >= ballTop && playerTop <= ballBottom) {
+        $('.player').addClass('goodbye')
+    }
 }
